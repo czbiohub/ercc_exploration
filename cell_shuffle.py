e9289b21d70f3547d78d998e3fd5853d06c80b58
Ubuntu <lincoln.harris@czbiohub.org>
2019-05-14 20:56:05 +0000

    changes to cellshuffle

--- cell_shuffle.py
+++ cell_shuffle.py
@@ -1,20 +1,26 @@
 import os
+import pandas as pd
 
 epiCellsAll = os.listdir('epiCellsAll')
-naive_cells = pd.read_csv('naive_cells.csv')
-pd_cells = pd.read_csv('pd_cells.csv')
-pr_cells = pd.read_csv('pr_cells.csv')
+
+naive_cells = pd.read_csv('naive_cells.csv', header=None, names = ['cell_id'])
+pd_cells = pd.read_csv('pd_cells.csv', header=None, names = ['cell_id'])
+pr_cells = pd.read_csv('pr_cells.csv', header=None, names = ['cell_id'])
+
+naive_cells = list(naive_cells['cell_id'])
+pd_cells = list(pd_cells['cell_id'])
+pr_cells = list(pr_cells['cell_id'])
 
 for cell in epiCellsAll:
     if cell in naive_cells:
         cmd = 'cp epiCellsAll/' + cell + '/ ' + 'TN_tumor/ --recursive'
-        print(cmd)
-        #os.system(cmd)
+        #print(cmd)
+        os.system(cmd)
     elif cell in pd_cells:
         cmd = 'cp epiCellsAll/' + cell + '/ ' + 'PD_tumor/ --recursive'
-        print(cmd)
-        #os.system(cmd)
+        #print(cmd)
+        os.system(cmd)
     elif cell in pr_cells:
         cmd = 'cp epiCellsAll/' + cell + '/ ' + 'PER_tumor/ --recursive'
-        print(cmd)
-        #os.system(cmd)
+        #print(cmd)
+        os.system(cmd)
